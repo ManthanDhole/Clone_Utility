2eac806975db75fbeff2e9d3408c33ba12ae47cb
mdhole <manthan.dhole@infor.com>
2023-09-18 17:01:51 +0530

    Initial Commit

---
+++ Clone.cs
@@ -0,0 +1,15 @@
+namespace Clone_Utility_
+{
+    public partial class Clone : Form
+    {
+        public Clone()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

53be240ac71040874f6849e442166876d30dc206
mdhole <manthan.dhole@infor.com>
2023-09-18 19:58:31 +0530

    Code for clone

--- Clone.Designer.cs
+++ Clone.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.txtWebURL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBranch = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnClone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Location";
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(126, 24);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(264, 31);
+            this.txtLocation.TabIndex = 1;
+            // 
+            // txtWebURL
+            // 
+            this.txtWebURL.Location = new System.Drawing.Point(126, 61);
+            this.txtWebURL.Name = "txtWebURL";
+            this.txtWebURL.Size = new System.Drawing.Size(264, 31);
+            this.txtWebURL.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Web URL";
+            // 
+            // txtBranch
+            // 
+            this.txtBranch.Location = new System.Drawing.Point(126, 98);
+            this.txtBranch.Name = "txtBranch";
+            this.txtBranch.Size = new System.Drawing.Size(264, 31);
+            this.txtBranch.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Branch";
+            // 
+            // btnClone
+            // 
+            this.btnClone.Location = new System.Drawing.Point(48, 145);
+            this.btnClone.Name = "btnClone";
+            this.btnClone.Size = new System.Drawing.Size(319, 34);
+            this.btnClone.TabIndex = 6;
+            this.btnClone.Text = "Clone";
+            this.btnClone.UseVisualStyleBackColor = true;
+            this.btnClone.Click += new System.EventHandler(this.btnClone_Click);
+            // 
+            // Clone
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(428, 204);
+            this.Controls.Add(this.btnClone);
+            this.Controls.Add(this.txtBranch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtWebURL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.label1);
+            this.Name = "Clone";
+            this.Text = "Clone";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtLocation;
+        private TextBox txtWebURL;
+        private Label label2;
+        private TextBox txtBranch;
+        private Label label3;
+        private Button btnClone;
     }
 }
--- Clone.cs
+++ Clone.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Clone_Utility_
 {
     public partial class Clone : Form
@@ -7,9 +9,64 @@ namespace Clone_Utility_
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public void button1_Click(object sender, EventArgs e)
+        {
+             
+        }
+        public void StartCloning(string pathToClone, string webURL, string branchName)
+        {
+            string command = $"git clone -b {branchName} {webURL}";
+            Process prc;
+            string dateTime = (Convert.ToString(DateTime.Now)).Replace("/", "-").Replace(":", "-").Replace(" ", "_");
+            string folderCreated = $"{branchName}-{dateTime}";
+            var p = new ProcessStartInfo
+            {
+                WorkingDirectory = pathToClone,
+                UseShellExecute = false,
+                FileName = @"C:\Windows\System32\cmd.exe"
+            };
+
+            prc = new Process
+            {
+                StartInfo = p
+            };
+
+            prc.StartInfo.ArgumentList.Add($"/c cd {pathToClone} && mkdir {folderCreated} && cd {folderCreated} && {command}");
+            prc.Start();
+            prc.WaitForExit();
+            Console.WriteLine();
+        }
+
+        private void txtLocation_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnClone_Click(object sender, EventArgs e)
+        {
+            string pathToClone = txtLocation.Text;
+            string WebURL = txtWebURL.Text;
+            string branchName = txtBranch.Text;
+
+            if (string.IsNullOrEmpty(pathToClone))
+            {
+                pathToClone = @"C:\Portal\Cloned_Projects";
+            }
+            if(!Directory.Exists(pathToClone))
+            {
+                Directory.CreateDirectory(pathToClone);
+            }
+            if (string.IsNullOrEmpty(WebURL))
+            {
+                MessageBox.Show("Enter the Web URL for project to clone", "Empty Web URL" ,MessageBoxButtons.OK);
+            }
+            if(string.IsNullOrEmpty(branchName))
+            {
+                MessageBox.Show("Enter the Branch Name", "Empty Branch Name", MessageBoxButtons.OK);
+            }
+
+            StartCloning(pathToClone, WebURL, branchName);
+            MessageBox.Show($"Project Cloned Successfully!", "Completed");
         }
     }
 }
